b7d737539d426e16b16a635ae95d776b9574e009
chrismslist <cgrab713@gmail.com>
2024-06-10 17:18:56 -0400

    updated and removed java files, fixed redundencies, remapped tables, updated front end names and api calls

---
+++ Relational Schema (SQL Scripts)/db_setup.sql
@@ -0,0 +1,49 @@
+-- Database Requirements
+
+/* User requirements
+ * The application should support two types of users.
+ * 1.) Customers: customers can look at forums, add comments, and posts.
+ * 2.) Staff Members: Staff can add and modify forums, and delete comments and posts.
+ */
+
+/*
+-- Create the Users table if it doesn't exist
+CREATE TABLE IF NOT EXISTS Users (
+    user_id SERIAL PRIMARY KEY,
+    user_type VARCHAR(20) NOT NULL, -- The application should support two types of users
+    name VARCHAR(100) NOT NULL, -- For each customer, the application should record the NAME of the customer
+);
+*/
+
+CREATE TABLE IF NOT EXISTS Customer (
+    customer_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+-- Create the StaffMembers table
+CREATE TABLE IF NOT EXISTS Staff_Member (
+    staff_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+-- Create the Products table
+CREATE TABLE IF NOT EXISTS Forum (
+    forum_id SERIAL PRIMARY KEY,
+    category VARCHAR(50) NOT NULL,
+    forum_name VARCHAR(100) NOT NULL,
+    forum_description TEXT NOT NULL
+);
+
+-- Create the Comments table
+CREATE TABLE IF NOT EXISTS Comment (
+    comment_id SERIAL PRIMARY KEY,
+    comment_text TEXT NOT NULL,
+    customer_id SERIAL,
+    forum_id SERIAL,
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
+    FOREIGN KEY (forum_id) REFERENCES Forum(forum_id)
+);
