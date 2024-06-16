e563aad29d520aea73c244e733f7787a0b86d7de
chrismslist <cgrab713@gmail.com>
2024-06-16 15:47:53 -0400

    remove duplicates

--- Application (Implementation)/web-site/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // Your main App component
-
-
-const root = document.getElementById('root');
-const appRoot = ReactDOM.createRoot(root);
-appRoot.render(<App />);
-
