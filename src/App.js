7ba8c63c85ece5edf04f2001a40227893541de22
chrismslist <cgrab713@gmail.com>
2024-06-27 23:39:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,29 +6,6 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import { Helmet } from 'react-helmet';
 
-
-const LoadingSpinner = () => {
-  const { loading } = useContext(LoadingProvider);
-  return loading ? (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        backgroundColor: 'rgba(255, 255, 255, 0.7)',
-        zIndex: 9999
-      }}
-    >
-      <CircularProgress />
-    </Box>
-  ) : null;
-};
-
 const App = () => {
   return (
     <div>
@@ -37,9 +14,7 @@ const App = () => {
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6853203533491695"
                 crossorigin="anonymous"></script>
       </Helmet>
-      
       <LoadingProvider>
-        <LoadingSpinner />
         <PageRoutes />
         <Home />
       </LoadingProvider>
